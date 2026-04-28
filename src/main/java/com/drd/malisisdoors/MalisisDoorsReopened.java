5d4f27ff1f8a12e0d330547f872a66d46d8b24a6
Dandi2k8 <drd.construction30@gmail.com>
2026-04-28 16:13:36 -0700

    Some more changes

--- src/main/java/com/drd/malisisdoors/MalisisDoorsReopened.java
+++ src/main/java/com/drd/malisisdoors/MalisisDoorsReopened.java
@@ -12,30 +12,8 @@ import net.neoforged.neoforge.event.server.ServerStartingEvent;
 public class MalisisDoorsReopened {
     public static final String MOD_ID = "malisisdoors";
 
-//    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MOD_ID);
-//    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MOD_ID);
-//    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
-//
-//    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", p -> p.mapColor(MapColor.STONE));
-//    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
-//
-//    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", p -> p.food(new FoodProperties.Builder()
-//            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
-//
-//    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-//            .title(Component.translatable("itemGroup.malisisdoors"))
-//            .withTabsBefore(CreativeModeTabs.COMBAT)
-//            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-//            .displayItems((parameters, output) -> {
-//                output.accept(EXAMPLE_ITEM.get());
-//            }).build());
-
     public MalisisDoorsReopened(IEventBus modEventBus, ModContainer modContainer) {
         modEventBus.addListener(this::commonSetup);
-
-//        BLOCKS.register(modEventBus);
-//        ITEMS.register(modEventBus);
-//        CREATIVE_MODE_TABS.register(modEventBus);
 
         NeoForge.EVENT_BUS.register(this);
     }
